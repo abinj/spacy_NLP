e1c2b13b6947afc9339e8aa46722b4e02e418d2d
abinj <abinmj656@gmail.com>
2020-02-24 01:09:57 -0500

    Syntactic dependencies and NER done

--- ner_practice.py
+++ ner_practice.py
@@ -1,3 +1,4 @@
+import spacy
 from spacy.lang.de import German
 from spacy.lang.en import English
 from spacy.lang.zh import Chinese
@@ -33,3 +34,24 @@ nlp = Chinese()
 nlp.use_jieba = False
 doc = nlp("民主党总统候选人领先者参议员伯尼·桑德斯在周日播出的一次采访中说，如果中国决定打击台湾，他将采取军事行动。")
 print_lexical_attributes(doc)
+
+
+
+print("\n\nStatistical Models")
+# POS, Syntactic Dependencies, Named Entities
+#Make sure you downloaded the model using command 'python -m spacy download en_core_web_sm'
+nlp = spacy.load('en_core_web_sm')
+doc = nlp("Why do you stay in prison when the door is so wide open?")
+
+#pos_ is part-of-speech , dep_ isdepecdency label and head.text returns syntactic head token
+for token in doc:
+    print(token.text, token.pos_, token.dep_, token.head.text)
+
+print("\n\n")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+#predicting Named Entities
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+
+
+
